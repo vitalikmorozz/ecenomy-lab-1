936a9b7eabdc2575cf2e05f62ff82c6e3e285b3d
vitalikmorozz <42621257+vitalikmorozz@users.noreply.github.com>
2022-10-26 22:28:49 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sel = <T extends HTMLElement>(selector: string) =>
+  document.querySelector<T>(selector)!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const form = sel<HTMLFormElement>('#calculate-form');
+const input = sel<HTMLInputElement>('#kloc');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert(input.value);
+});
